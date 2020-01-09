e4ddfb5446006e0320487f6e676538a88ca5e3c7
zantrash <fdhiyaul163@gmail.com>
2020-01-09 02:51:22 +0000

    AnyKernel3

---
+++ AnyKernel/anykernel.sh
@@ -0,0 +1,35 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Scrapper Kernel by @zantrash
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=riva
+device.name2=Redmi 5A
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+dump_boot;
+
+write_boot;
+## end install
+
